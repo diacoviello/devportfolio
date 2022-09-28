8bb1732307cc7394bef9a8b4b7645a9ef7f599da
Dave <diacovmusic@gmail.com>
2022-09-28 14:13:41 -0400

    first branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { BrowserRouter } from 'react-router-dom';
+import FallbackSpinner from './components/FallbackSpinner';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import endpoints from './constants/endpoints';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import useDarkMode from 'use-dark-mode';
-import NavBar from './components/NavBar';
 import AppContext from './AppContext';
 import MainApp from './MainApp';
 import GlobalStyles from './theme/GlobalStyles';
@@ -14,13 +17,26 @@ function App() {
   window.matchMedia = null;
   const darkMode = useDarkMode(true);
 
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(endpoints.routes, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => err);
+  }, []);
+
+
   return (
     <AppContext.Provider value={{ darkMode }}>
       <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
         <GlobalStyles />
         <div className="App">
-          <NavBar />
-          <MainApp />
+          <Router basename={process.env.PUBLIC_URL}>
+            <MainApp />
+          </Router>
         </div>
       </ThemeProvider>
     </AppContext.Provider>
--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,46 +1,45 @@
 import React, { useState, useEffect, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Route } from 'react-router-dom';
 import FallbackSpinner from './components/FallbackSpinner';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import endpoints from './constants/endpoints';
 
 function MainApp() {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        fetch(endpoints.routes, {
-            method: 'GET',
-        })
-            .then((res) => res.json())
-            .then((res) => setData(res))
-            .catch((err) => err);
-    }, []);
+  useEffect(() => {
+    fetch(endpoints.routes, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => err);
+  }, []);
 
-    return (
-        <div className="MainApp">
-            <Router className="main">
-                <Switch>
-                    <Suspense fallback={<FallbackSpinner />}>
-                        <Route exact path={['/']} component={Home} />
-                        {data
-                            && data.sections.map((route) => {
-                                const SectionComponent = React.lazy(() => import('./components/' + route.component));
-                                return (
-                                    <Route
-                                        key={route.headerTitle}
-                                        path={route.path}
-                                        component={() => (
-                                            <SectionComponent header={route.headerTitle} />
-                                        )}
-                                    />
-                                );
-                            })}
-                    </Suspense>
-                </Switch>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="main">
+      <NavBar />
+      <>
+        <Suspense fallback={<FallbackSpinner />}>
+          <Route exact path="/" component={Home} />
+          {data
+            && data.sections.map((route) => {
+              const SectionComponent = React.lazy(() => import('./components/' + route.component));
+              return (
+                <Route
+                  key={route.headerTitle}
+                  path={route.path}
+                  component={() => (
+                    <SectionComponent header={route.headerTitle} />
+                  )}
+                />
+              );
+            })}
+        </Suspense>
+      </>
+    </div>
+  );
 }
 
 export default MainApp;
