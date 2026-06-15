d9e92e5b4b6f488c07c085261b5816b4b989b716
Dave <diacovmusic@gmail.com>
2026-06-15 00:02:40 -0400

    fix home 2

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,12 +1,14 @@
+const publicUrl = process.env.PUBLIC_URL || '';
+
 const endpoints = {
-  navbar: 'profile/navbar.json',
-  routes: 'profile/routes.json',
-  home: 'profile/home.json',
-  social: 'profile/social.json',
-  about: 'profile/about.json',
-  education: 'profile/education.json',
-  experiences: 'profile/experiences.json',
-  projects: 'profile/projects.json',
+  navbar: `${publicUrl}/profile/navbar.json`,
+  routes: `${publicUrl}/profile/routes.json`,
+  home: `${publicUrl}/profile/home.json`,
+  social: `${publicUrl}/profile/social.json`,
+  about: `${publicUrl}/profile/about.json`,
+  education: `${publicUrl}/profile/education.json`,
+  experiences: `${publicUrl}/profile/experiences.json`,
+  projects: `${publicUrl}/profile/projects.json`,
 };
 
 export default endpoints;
